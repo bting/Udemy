0608fdd313a3122718ba7826fded9ab5c3e81eac
Tingting Bao <baoting.tina@gmail.com>
2018-03-02 17:13:51 -0800

    second commit

--- Color Game Project/colorGame.js
+++ Color Game Project/colorGame.js
@@ -8,19 +8,34 @@ var colors = [
 ]
 
 var displayColor = document.getElementById("displayColor");
-var pickedColor = colors[0];
+var pickedColor = pickColor();
 displayColor.textContent = pickedColor;
 var squares = document.querySelectorAll(".square");
+var messageDisplay = document.querySelector("#message");
+
 for(var i = 0; i < squares.length; i++) {
   // add initial color to squares
-  squares[i].style.background = colors[i];
+  squares[i].style.backgroundColor = colors[i];
   // add event listener to squares
   squares[i].addEventListener("click", function() {
-    var clickecdColor = this.style.background;
+    var clickecdColor = this.style.backgroundColor;
     if (clickecdColor === pickedColor) {
-      alert("correct");
+      changeColor(pickedColor);
+      messageDisplay.textContent = "Correct";
     } else {
-      alert("wrong!!");
+      this.style.backgroundColor = "#232323";
+      messageDisplay.textContent = "Try Again";
     }
   });
+}
+
+function changeColor(color) {
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.backgroundColor = color;
+  }
+}
+
+function pickColor() {
+  var index = Math.floor(Math.random() * colors.length);
+  return colors[index];
 }
