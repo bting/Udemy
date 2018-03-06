b31b450e6d9a1c1be87f14e8fafaac311d8d3c84
Tingting Bao <baoting.tina@gmail.com>
2018-03-06 09:22:53 -0800

    adding styles

--- Color Game Project/colorGame.js
+++ Color Game Project/colorGame.js
@@ -61,7 +61,7 @@ resetButton.addEventListener("click", function() {
   for (var i = 0; i < colors.length; i++) {
     squares[i].style.backgroundColor = colors[i];
   }
-  h1.style.backgroundColor = "#232323";
+  h1.style.backgroundColor = "steelblue";
 });
 
 for(var i = 0; i < squares.length; i++) {
