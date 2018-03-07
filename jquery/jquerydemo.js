fcce469940f1eae012f40fb9c524b5dfa015476c
Tingting Bao <baoting.tina@gmail.com>
2018-03-06 18:02:37 -0800

    jquery practice

---
+++ jquery/jquerydemo.js
@@ -0,0 +1,5 @@
+$("div").css("background", "purple");
+$(".highlight").css("width", "200px");
+$("#third").css("border", "4px solid orange");
+// div:first-of-type is faster than div:first,cause first-of-type is js build-in
+$("div:first-of-type").css("color", "pink");
