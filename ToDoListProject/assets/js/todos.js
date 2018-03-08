12cb68cef6c6b4e5c65de93cecccd689a3ef6eac
Tingting Bao <baoting.tina@gmail.com>
2018-03-07 16:19:44 -0800

    create new todo list and change the listener of li and span

--- ToDoListProject/assets/js/todos.js
+++ ToDoListProject/assets/js/todos.js
@@ -1,13 +1,25 @@
 // Check Off Specific Todos By Clicking
-$("li").click(function(){
+// use on to add eventListener to future li.
+// when the li inside ul is clicked, call this function(works for future li also)
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
 });
 
 // Click on X to delete ToDo
-$("span").click(function(event) {
+$("ul").on("click", "span", function(event) {
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   // prevent the event from bubbling up the DOM tree
   event.stopPropagation();
+})
+
+// create new todo list and appear on screen
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13) {
+    var todoStr = $(this).val();
+    // make input becomes empty again
+    $(this).val("");
+    $("ul").append("<li><span>X</span> " + todoStr + "</li>");
+  }
 })
