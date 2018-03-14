24c0298d5b0a255c917b46dae01cbe0e9bc94c50
Tingting Bao <baoting.tina@gmail.com>
2018-03-14 13:03:42 -0700

    Add search function

--- express/movieApp/index.js
+++ express/movieApp/index.js
@@ -1,15 +1,23 @@
 var express = require("express");
 var request = require("request");
 var app = express();
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res) {
+    res.render("search");
+});
 
 app.get("/results", function(req, res) {
-    request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", function(err, response, body) {
+    var query = req.query.search;
+    var url = "http://www.omdbapi.com/?s=" + query+"&apikey=thewdb";
+    request(url, function(err, response, body) {
         if(!err && response.statusCode == 200) {
-            var results = JSON.parse(body);
-            res.send(results["Search"][0]["Title"]);
+            var data = JSON.parse(body);
+            res.render("results", {data: data});
         }
-    })
-})
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Movie app started!");
 });
