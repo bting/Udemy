0adfebd63b4f76ecae21b24c327f68a590940b94
Tingting Bao <baoting.tina@gmail.com>
2018-03-15 14:43:08 -0700

    create post and new request

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,5 +1,7 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 var campgrounds = [
@@ -21,6 +23,18 @@ app.get("/", function(req, res) {
 app.get("/campgrounds", function(req, res) {
     res.render("campgrounds", {campgrounds: campgrounds});
 });
+
+app.post("/campgrounds", function(req, res){
+    var name = req.body.name;
+    var image = req.body.image;
+    var newCampground = {name: name, image: image};
+    campgrounds.push(newCampground);
+    res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+    res.render("new");
+})
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("YelpCamp server has started!!");
