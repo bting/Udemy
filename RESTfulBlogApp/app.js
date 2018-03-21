8d50f207b6d6d3150eee822926ec4398972ac1f4
Tingting Bao <baoting.tina@gmail.com>
2018-03-21 09:53:52 -0700

    style index and new page and create show page to show information of a single blog

--- RESTfulBlogApp/app.js
+++ RESTfulBlogApp/app.js
@@ -37,6 +37,26 @@ app.get("/blogs/new", function(req, res) {
     res.render("new");
 });
 
+app.post("/blogs", function(req, res) {
+    Blog.create(req.body.blog, function(err, newBlog) {
+        if(err) {
+            res.render("new");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running...");
 });
